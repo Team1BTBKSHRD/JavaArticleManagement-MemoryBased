28b94ed4b92537365808a8fdea37ff74898e7b93
SarinRupp <hemsarin.rupp@gmail.com>
2015-05-22 15:29:42 +0700

    I add new  function Add

--- src/AddDemo.java
+++ src/AddDemo.java
@@ -1,28 +1,60 @@
+import java.util.*;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
+
 public class AddDemo {
-		public void input(){	
+
+	public void validateDatas(ArrayList<Article> lsin,String author, String title, String content) {
+		String publishDate = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss")
+				.format(new Date());		
+		if (author.isEmpty() || title.isEmpty()) {
+			System.out.print("Value null");
+		} else {
+			lsin.add(new Article(author, title, content, publishDate));			
+		}		
+	}
+	public boolean checkConfrim(String check){
+		boolean ch = true;
+		if(check=="Y" || check=="y"){
+			 ch=true;
+		}
+		else if(check=="N" || check=="n"){
+			ch=false;
 			
-			Scanner put = new Scanner(System.in); 
-			String content = "";
-			String title="";
-			String author="";
-			String date="";
+		}
+		return ch;
+	}
+	public void input() {
+		ArrayList<Article> lsout = new ArrayList<Article>();
+		lsout.add(new Article("A", "A", "A", "A"));
+		Scanner put = new Scanner(System.in);
+		String content = "";
+		String title = "";
+		String author = "";
+		String check="";
+//		input n value
+		while (true) {			
 			System.out.print("Please Enter Author Name: ");
-			author=put.next();
+			author = put.nextLine();
 			System.out.print("Please Enter Titile Name: ");
-			title=put.next();
-			System.out.println("Please Enter Content: "); 			
-			while (put.hasNext()) { 
-			content += put.nextLine(); 
-			if (content.endsWith(".")) 
-			break; 
-			}	
-			
-			
-			Article Add=new Article(author, title, content,date);
+			title = put.nextLine();
+			System.out.println("Please Enter Content: ");
+			while (put.hasNext()) {
+				content += put.nextLine();
+				if (content.endsWith("."))
+					break;
+			}		
+			this.validateDatas(lsout,author.trim(), title.trim(), content.trim());
+			System.out.print("You want to continues: (Y/N): ");
+			check=put.nextLine();
+			System.out.print(this.checkConfrim(check));
 		}
-		public static void main(String[] args) {
-			AddDemo a = new AddDemo();
-			a.input();
-		}
+//		end of value
+		
+	}
+
+	public static void main(String[] args) {
+		AddDemo a = new AddDemo();
+		a.input();
+	}
 }
