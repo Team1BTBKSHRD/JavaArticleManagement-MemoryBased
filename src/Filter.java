0b07648070cc9834dce4257cdfee9eec858d7ebf
SarinRupp <hemsarin.rupp@gmail.com>
2015-05-22 10:45:22 +0700

    Delete search by id

--- src/Filter.java
+++ src/Filter.java
@@ -8,7 +8,7 @@ public class Filter {
 		HashSet<Integer> indexs = new HashSet<Integer>();
 		//Linear Search
 		for(int i = 0; i < list.size(); i ++){
-			if((list.get(i).getTitle().contains(target) || (list.get(i).getAuthor().contains(target) || (list.get(i).getIdToString().startsWith(target))))){
+			if((list.get(i).getTitle().contains(target) || (list.get(i).getAuthor().contains(target) ))){
 				indexs.add(i);
 			}
 		}
