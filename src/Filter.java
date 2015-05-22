c49a8bf9f4a65154b808ffc85d4ecb712ae7b5fd
Channa Rours <rourschanna@gmail.com>
2015-05-22 12:57:10 +0700

    add updateWithIndex() to return index of element when user enter id of that element......

--- src/Filter.java
+++ src/Filter.java
@@ -1,17 +1,25 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 /*
  * Author : Elit, Channa (Search Mission)
  */
 public class Filter {
-	public static HashSet<Integer> searchContains(ArrayList<Article> list, String target){
-		HashSet<Integer> indexs = new HashSet<Integer>();
+	private static HashMap<String, Integer> updateMap = new HashMap<String,Integer>();
+	public static ArrayList<Integer> searchContains(ArrayList<Article> list, String target){
+		ArrayList<Integer> indexs = new ArrayList<Integer>();
 		//Linear Search
 		for(int i = 0; i < list.size(); i ++){
-			if((list.get(i).getTitle().contains(target) || (list.get(i).getAuthor().contains(target) ))){
+			if((list.get(i).getTitle().contains(target) || (list.get(i).getAuthor().contains(target)))){
 				indexs.add(i);
+				updateMap.put(list.get(i).getId()+"", i);
 			}
 		}
 		return indexs;
+	}
+	
+	// Get index of id want to update
+	public static int updateWithIndex(String id){
+		return updateMap.get(id);
 	}
 }
