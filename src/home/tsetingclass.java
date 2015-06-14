272356ca0f9fc358872dd2e4b3c56e11c6c2e752
kepisal <kepisal@gmail.com>
2015-06-14 22:36:59 +0700

    this is testing

--- src/home/tsetingclass.java
+++ src/home/tsetingclass.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class tsetingclass {
 	public static void main(String[] args) {
 		System.out.println("home");
+		System.out.println("home");
+		System.out.println("home");
+		System.out.println("home");
 	}
 }
