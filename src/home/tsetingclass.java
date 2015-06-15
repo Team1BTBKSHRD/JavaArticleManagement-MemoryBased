0cc34267f5310009e6fd40b3c9ca65cc824629bb
elit <chhornelit@gmail.com>
2015-06-15 15:33:48 +0700

    convert to eclipse project

--- src/home/tsetingclass.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class tsetingclass {
-	public static void main(String[] args) {
-		System.out.println("home");
-		System.out.println("home");
-		System.out.println("home");
-		System.out.println("home");
-	}
-}
